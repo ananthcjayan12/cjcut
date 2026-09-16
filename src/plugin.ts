21ba22a704e4343b298fb2882f5d51435de687fc
ananthcjayan12 <123536374+ananthcjayan12@users.noreply.github.com>
2026-09-17 00:57:05 +0530

    Add CJCut plugin entry point

---
+++ src/plugin.ts
@@ -0,0 +1,11 @@
+import './styles.css'
+export { CJCutEditor, START_PROJECT } from './App'
+export type {
+  CJCutEditorProps,
+  Project as CJCutProject,
+  Track as CJCutTrack,
+  Clip as CJCutClip,
+  TrackType as CJCutTrackType,
+  MediaKind as CJCutMediaKind,
+  MediaAsset as CJCutMediaAsset,
+} from './App'
